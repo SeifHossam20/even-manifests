4f180134c610869cc896da97118a1d7e42f2b50b
ZYLEX <86944731+Badmaneers@users.noreply.github.com>
2024-10-05 11:18:34 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,7 +5,7 @@ rm -rf kernel/realme/mt6768
 git clone --recurse-submodules https://github.com/Badmaneers/kernel_realme_mt6768.git kernel/realme/mt6768
 # Clone Vendor
 rm -rf vendor/realme/even
-git clone https://github.com/eun0115/vendor_realme_even.git -b twelve-prebuilt vendor/realme/even
+git clone https://github.com/Badmaneers/android_vendor_realme_even.git -b twelve-oss vendor/realme/even
 # Clone Device Tree
 rm -rf device/realme/even
 git clone https://github.com/Badmaneers/device_realme_even.git -b thirteen-oss device/realme/even
