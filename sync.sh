30a3f4c846ce6c36dc1bf539dcabd0958bcfa6ee
Seif Hossam <seifhossam862@gmail.com>
2024-10-07 16:53:52 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 # Clone Kernel
-git clone --recurse-submodules https://github.com/Badmaneers/kernel_realme_mt6768.git kernel/realme/mt6768
+git clone --recurse-submodules https://github.com/eun0115/kernel_realme_mt6768.git kernel/realme/mt6768
 # Clone Vendor
 git clone https://github.com/eun0115/vendor_realme_even.git -b twelve-prebuilt vendor/realme/even
 # Clone Device Tree
-git clone https://github.com/SeifHossam20/device_realme_even.git -b 13 device/realme/even
+git clone https://github.com/eun0115/device_realme_even.git -b twelve-prebuilt device/realme/even
 # Clone Vendor-Ims
-git clone https://github.com/https://github.com/SeifHossam20/android_vendor_realme_even-ims.git -b thirteen-rmui2 vendor/realme/even-ims
-# Clone Lineage Tree
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-20 hardware/mediatek
-# Clone Lineage sepolicy
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 device/mediatek/sepolicy_vndr
+git clone https://github.com/SeifHossam20/android_vendor_realme_even-ims.git -b twelve-rmui2 vendor/realme/even-ims
 # Clone Realme-Dirac
-git clone https://github.com/HyperTeam/android_packages_apps_RealmeParts.git -b lineage-20 packages/apps/RealmeParts
+git clone https://github.com/HyperTeam/android_packages_apps_RealmeParts.git -b lineage-19.1 packages/apps/RealmeParts
 # Clone Realme-Parts
-git clone https://github.com/techyminati/android_packages_apps_RealmeDirac -b lineage-20 packages/apps/RealmeDirac
+git clone https://github.com/techyminati/android_packages_apps_RealmeDirac.git -b lineage-19.1 packages/apps/RealmeDirac
